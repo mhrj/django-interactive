43e1ce13301f4f6feff1259d4873dc08e588d407
Mh <mhrj007846@gmail.com>
2025-05-20 23:04:15 +0330

    docker problems fixed

--- Dockerfile.r
+++ Dockerfile.r
@@ -1,6 +1,28 @@
-FROM r-base:4.3.1
+# FROM r-base:4.3.1
 
-RUN R -e "install.packages('Rserve', repos='http://cran.rstudio.com/')"
+# RUN R -e "install.packages(c('Rserve','zoo','syuzhet'), repos='http://cran.rstudio.com/')"
+
+# COPY start_rserve.sh /start_rserve.sh
+# RUN chmod +x /start_rserve.sh
+
+# CMD ["/start_rserve.sh"]
+
+
+FROM r-base:latest
+
+# Install system dependencies needed to compile R packages (especially Rserve)
+RUN apt-get update && apt-get install -y \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libxml2-dev \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install R packages
+RUN R -e "install.packages(c('Rserve', 'zoo', 'syuzhet'), repos='http://cran.rstudio.com/')"
+
+# Expose Rserve port
+EXPOSE 6312
 
 COPY start_rserve.sh /start_rserve.sh
 RUN chmod +x /start_rserve.sh
